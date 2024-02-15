e4e18da8182ccda42af063c7c0204ec917405aaa
Antony_Chagas <antony.tiagor445@gmail.com>
2024-02-14 23:17:25 -0300

    Criação do arquivo CSS

--- public/data.php
+++ public/data.php
@@ -2,9 +2,11 @@
 
 $productName = 'Anilha de Musculação da Mon';
 $launch = 'Lançamento';
-$value = ' 0';
-$price = 'por R$' . $value;
-$price2 = 'ou 12x de R$ ' . ($value * 1.12) / 12 . 'com juros Cartão - Vind';
+$value = 1000;
+$price = 'por R$ ' . $value;
+$price2 = 'ou 12x de R$ ' . number_format(($value * 1.12) / 12, 2, ',', '.'). ' com juros Cartão - Vindi';
+
 $formsOfpaagmwento = 'Ver todas as formas de pagamento';
 $addToList = 'Adicioar à lista';
+
 
--- public/index.php
+++ public/index.php
@@ -6,23 +6,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+  
     <h1> <?php echo "$productName"; ?></h1>
+
+    <div id="cotainerA">
+     <img src="img/anilha.png">  
+    </div>
+
+    <div id="cotainerB">
     <h3> <?php echo "$launch"; ?></h3>
+    <h3> <?php echo "$price"; ?></h3>
+    <h3> <?php echo "$price2"; ?></h5>
+    <h5> <?php echo "$formsOfpaagmwento"; ?></h5>
+
+    <div id="cotainerC">
     <form method="POST" action="data.php">
         <label>Quantidade:</label>
         <input type="int" name="Quantidade">
+        <input type="submit" value="Comprar"
     </form>
+    </div>
 
+    <div>
+
+    </div>
     <form method="POST" action="data.php">
     <br> Simulador de frete <br> 
         <label>CEP:</label>
         <input type="int" name="CEP" size="10"> -
         <input type="int" name="Final CEP" size="2" >
-        
+        <input type="submit" value="Calcular frete"
     </form>
+    </div>
+    
+    
+
+    
+    
     
 </body>
 </html> 
